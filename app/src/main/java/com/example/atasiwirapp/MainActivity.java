27abb8920ae69911a65108003d75328547939269
scoobymonza <msultanfahrezi@gmail.com>
2021-12-05 11:49:05 +0700

    test

--- app/src/main/java/com/example/atasiwirapp/MainActivity.java
+++ app/src/main/java/com/example/atasiwirapp/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.atasiwirapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView asu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        asu = findViewById(R.id.imageView);
     }
 }
