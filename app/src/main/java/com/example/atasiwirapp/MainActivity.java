7728ddf0e2d7c1a2152b041c24ec959c9312ef93
Nabil Lesmana <lesmananabil@gmail.com>
2021-12-05 22:31:20 +0700

    fix default layout

--- app/src/main/java/com/example/atasiwirapp/MainActivity.java
+++ app/src/main/java/com/example/atasiwirapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_wisata);
+        setContentView(R.layout.activity_main);
 
         asu = findViewById(R.id.imageView);
         listRecyclerView = findViewById(R.id.recyclerView);
