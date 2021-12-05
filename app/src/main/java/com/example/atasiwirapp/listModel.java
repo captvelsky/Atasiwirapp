b4b6faceb120b3b6f9303f194be7abc8156245af
captvelsky <lesmananabil@gmail.com>
2021-12-05 19:44:18 +0700

    create menu wisata (tanpa intent)

---
+++ app/src/main/java/com/example/atasiwirapp/listModel.java
@@ -0,0 +1,39 @@
+package com.example.atasiwirapp;
+
+public class listModel {
+
+    private String title, rating, desc;
+    private int img;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
---
+++ app/src/main/java/com/example/atasiwirapp/menuWisata.java
@@ -0,0 +1,13 @@
+package com.example.atasiwirapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menuWisata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_wisata);
+    }
+}
