41a18d35d362b7a12f2978e468aa0244b1cb7671
irfanjesia <irfanjesia31@gmail.com>
2021-12-06 11:09:48 +0700

    add review activity

--- app/src/main/java/com/example/atasiwirapp/ReviewActivity.java
+++ app/src/main/java/com/example/atasiwirapp/ReviewActivity.java
@@ -3,17 +3,12 @@ package com.example.atasiwirapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
-    ImageView asu;
+public class ReviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
-
-        asu = findViewById(R.id.imageView);
     }
 }
