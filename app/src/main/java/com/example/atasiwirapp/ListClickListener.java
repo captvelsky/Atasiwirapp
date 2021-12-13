99e8afa130e719acc5e2beb36745bae80513a9e1
Nabil Lesmana <lesmananabil@gmail.com>
2021-12-13 18:17:53 +0700

    create listWisata intent

---
+++ app/src/main/java/com/example/atasiwirapp/ListClickListener.java
@@ -0,0 +1,8 @@
+package com.example.atasiwirapp;
+
+import android.view.View;
+
+public interface ListClickListener {
+
+    void onListClickListener(View v, int position);
+}
--- app/src/main/java/com/example/atasiwirapp/listHolder.java
+++ app/src/main/java/com/example/atasiwirapp/listHolder.java
@@ -7,17 +7,29 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class listHolder extends RecyclerView.ViewHolder {
+public class listHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     ImageView listImgView;
     TextView listTitle, listRating, listDesc;
+    ListClickListener listClickListener;
 
-    public listHolder(@NonNull View itemView) {
+    listHolder(@NonNull View itemView) {
         super(itemView);
 
         this.listImgView = itemView.findViewById(R.id.cardImg);
         this.listTitle = itemView.findViewById(R.id.cardTitle);
         this.listRating = itemView.findViewById(R.id.cardRating);
         this.listDesc = itemView.findViewById(R.id.cardDesc);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        this.listClickListener.onListClickListener(v, getLayoutPosition());
+    }
+
+    public void setListClickListener(ListClickListener lc) {
+        this.listClickListener = lc;
     }
 }
