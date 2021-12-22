c261d548352ab3685a5aff1d781c3a726f11994a
Nabil Lesmana <lesmananabil@gmail.com>
2021-12-22 16:44:11 +0700

    add wisata list & fix home ui

--- app/src/main/java/com/example/atasiwirapp/MenuWisata.java
+++ app/src/main/java/com/example/atasiwirapp/MenuWisata.java
@@ -38,7 +38,7 @@ public class MenuWisata extends AppCompatActivity {
 
         m = new wisataModel();
         m.setImg(R.drawable.wisata_pantai_tiga_warna);
-        m.setTitle("Pantai Tiga Warnah");
+        m.setTitle("Pantai Tiga Warna");
         m.setRating("4.3");
         m.setDesc("Pantai yang memiliki gradasi tiga warna yang disebabkan oleh perbedaan kedalaman permukaannya.");
         models.add(m);
@@ -51,45 +51,38 @@ public class MenuWisata extends AppCompatActivity {
         models.add(m);
 
         m = new wisataModel();
-        m.setImg(R.drawable.wisata_bromo);
-        m.setTitle("Gunung Bromo");
-        m.setRating("4.7");
-        m.setDesc("Gunung Bromo adalah sebuah gunung berapi aktif yang  terkenal sebagai objek wisata utama di Jawa Timur.");
+        m.setImg(R.drawable.wisata_coban_rondo);
+        m.setTitle("Coban Rondo");
+        m.setRating("4.4");
+        m.setDesc("Coban Rondo adalah salah satu rekomendasi destinasi wisata bersama keluarga di akhir pekan.");
         models.add(m);
 
         m = new wisataModel();
-        m.setImg(R.drawable.wisata_pantai_tiga_warna);
-        m.setTitle("Pantai Tiga Warnah");
+        m.setImg(R.drawable.wisata_bukit_teletubbies);
+        m.setTitle("Bukit Teletubbies");
+        m.setRating("4.2");
+        m.setDesc("Bukit Teletubbies adalah tempat wisata yang area taman hijau dengan pemandangan Gunung Arjuno.");
+        models.add(m);
+
+        m = new wisataModel();
+        m.setImg(R.drawable.wisata_pantai_balekambang);
+        m.setTitle("Pantai Balekambang");
+        m.setRating("4.6");
+        m.setDesc("Pantai Balekambang menawarkan pemandangan bawah laut yang indah, dan disertai dengan berbagai kuliner.");
+        models.add(m);
+
+        m = new wisataModel();
+        m.setImg(R.drawable.wisata_paralayang_batu);
+        m.setTitle("Paralayang Batu");
+        m.setRating("4.4");
+        m.setDesc("Wisata paralayang di Batu menyajikan pemandangan alam perbukitan dan Kota Malang yanng indah.");
+        models.add(m);
+
+        m = new wisataModel();
+        m.setImg(R.drawable.wisata_taman_bunga_coban_talun);
+        m.setTitle("Taman Bunga Coban Talun");
         m.setRating("4.3");
-        m.setDesc("Pantai yang memiliki gradasi tiga warna yang disebabkan oleh perbedaan kedalaman permukaannya.");
-        models.add(m);
-
-        m = new wisataModel();
-        m.setImg(R.drawable.wisata_candi_badut);
-        m.setTitle("Candi Badut");
-        m.setRating("4.3");
-        m.setDesc("Candi Badut adalah sebuah candi yang terletak di kawasan Tidar, di bagian barat kota Malang.");
-        models.add(m);
-
-        m = new wisataModel();
-        m.setImg(R.drawable.wisata_bromo);
-        m.setTitle("Gunung Bromo");
-        m.setRating("4.7");
-        m.setDesc("Gunung Bromo adalah sebuah gunung berapi aktif yang  terkenal sebagai objek wisata utama di Jawa Timur.");
-        models.add(m);
-
-        m = new wisataModel();
-        m.setImg(R.drawable.wisata_pantai_tiga_warna);
-        m.setTitle("Pantai Tiga Warnah");
-        m.setRating("4.3");
-        m.setDesc("Pantai yang memiliki gradasi tiga warna yang disebabkan oleh perbedaan kedalaman permukaannya.");
-        models.add(m);
-
-        m = new wisataModel();
-        m.setImg(R.drawable.wisata_candi_badut);
-        m.setTitle("Candi Badut");
-        m.setRating("4.3");
-        m.setDesc("Candi Badut adalah sebuah candi yang terletak di kawasan Tidar, di bagian barat kota Malang.");
+        m.setDesc("Coban Talun memiliki taman bunga indah layaknya di Negeri Sakura yang cocok untuk dijadikan tempat berfoto.");
         models.add(m);
 
         return models;
